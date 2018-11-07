5858719918bd0c73dc074e2217fb1d1c60de16ce
annastachurska <anna.stachurska@gmail.com>
2018-11-07 12:55:06 +0100

    second smurf waling inside container

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const img = document.getElementsByClassName('aside_smurf');
-    //const reversed = document.querySelector('.rotated');
+    const reversed = document.getElementById('rotated');
+
     for (let i=0; i<img.length; i++) {
         img[i].style.backgroundPositionX = '0px';
         img[i].style.backgroundPositionY = '0px';
@@ -17,44 +18,19 @@ document.addEventListener('DOMContentLoaded', function(){
                 } else {
                     img[i].style.backgroundPositionY = y + 128 + 'px';
                 }
-
             } else {
                 img[i].style.backgroundPositionX = x + 128 + 'px';
             }
+
             if (i == 1) {
                 size = size + 5;
-                img[i].style.transform = 'rotateZ(90deg) translateX(' + size + 'px)';
-                if (size >= window.innerHeight) {
+                if (size + 128>= reversed.clientHeight) {
                     size = 0;
                 }
-
+                img[i].style.transform = 'rotateZ(90deg) translateX(' + size + 'px)';
             }
+
         }, 200);
     }
 
-
-
-
-
-    // const img = document.getElementById('smurf');
-    //
-    // img.style.backgroundPositionX = '0px';
-    // img.style.backgroundPositionY = '0px';
-    // setInterval(function(){
-    //     let x = parseInt(img.style.backgroundPositionX);
-    //     let y = parseInt(img.style.backgroundPositionY);
-    //
-    //     if (x + 128 + 128 > 512) {
-    //         img.style.backgroundPositionX ='0px';
-    //         if(y + 128 + 128 > 512) {
-    //             img.style.backgroundPositionY = '0px';
-    //         } else {
-    //             img.style.backgroundPositionY = y + 128 + 'px';
-    //         }
-    //
-    //     } else {
-    //         img.style.backgroundPositionX = x + 128 + 'px';
-    //     }
-    //
-    // }, 200);
 });
