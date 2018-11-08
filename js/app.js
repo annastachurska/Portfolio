6fd385a6d4b95b00f684c9d5dd94d6941a1c2421
annastachurska <anna.stachurska@gmail.com>
2018-11-08 12:31:30 +0100

    finished aside

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const img = document.getElementsByClassName('aside_smurf');
-    const reversed = document.getElementById('rotated');
+    const aside = document.getElementsByTagName('aside');
+    img[1].style.webkitTransform = 'rotateZ(90deg) scale(' + (aside[1].clientWidth/132) + ') translateY(' + (130 - aside[1].clientWidth) + 'px)';
+    img[1].style.transform = 'rotateZ(90deg) scale(' + (aside[1].clientWidth/132) + ') translateY(' + (130 - aside[1].clientWidth) + 'px)';
 
     for (let i=0; i<img.length; i++) {
         img[i].style.backgroundPositionX = '0px';
         img[i].style.backgroundPositionY = '0px';
         let size = 1;
-        setInterval(function(){
+        setInterval(() => {
             let x = parseInt(img[i].style.backgroundPositionX);
             let y = parseInt(img[i].style.backgroundPositionY);
 
@@ -23,14 +25,25 @@ document.addEventListener('DOMContentLoaded', function(){
             }
 
             if (i == 1) {
-                size = size + 5;
-                if (size + 128>= reversed.clientHeight) {
+                size += 5;
+                if (size + 128>= aside[1].clientHeight) {
                     size = 0;
                 }
-                img[i].style.transform = 'rotateZ(90deg) translateX(' + size + 'px)';
+                img[i].style.webkitTransform = 'rotateZ(90deg) translateX(' + size + 'px) scale(' + (aside[1].clientWidth/132) + ') translateY(' + (130 - aside[1].clientWidth) + 'px)';
+                img[i].style.transform = 'rotateZ(90deg) translateX(' + size + 'px) scale(' + (aside[1].clientWidth/132) + ') translateY(' + (130 - aside[1].clientWidth) + 'px)';
             }
 
         }, 200);
     }
+
+    window.addEventListener('resize', () => {
+        if (aside[0].clientWidth < 128) {
+            aside[0].querySelector('div').style.webkitTransform = 'translateY(30vh) scale(' + aside[0].clientWidth/128 + ') translateX(-' + (128 - aside[0].clientWidth) + 'px)';
+            aside[0].querySelector('div').style.transform = 'translateY(30vh) scale(' + aside[0].clientWidth/128 + ') translateX(-' + (128 - aside[0].clientWidth) + 'px)';
+        } else {
+            aside[0].querySelector('div').style.webkitTransform = 'translateY(30vh) scale(1)';
+            aside[0].querySelector('div').style.transform = 'translateY(30vh) scale(1)';
+        }
+    });
 
 });
